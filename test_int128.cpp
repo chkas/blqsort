c8580011d2db7934fda168a692bd99a5af8234b6
Christof Kaser <chkas@gmx.at>
2026-05-27 14:08:18 +0200

    Create test_int128.cpp

---
+++ test_int128.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+#include <algorithm>
+#include "blqs.h"
+
+constexpr int SIZE = 50000000;
+__int128 data[SIZE];
+
+double cputime() {
+    return (double)clock() / CLOCKS_PER_SEC;
+}
+int main() {
+    double t0;
+
+    for (int i = 0; i < SIZE; i++) data[i] = rand();
+    t0 = cputime();
+    std::sort(data, data + SIZE);
+    printf("std::sort %.2fs\n", cputime() - t0);
+
+    for (int i = 0; i < SIZE; i++) data[i] = rand();
+    t0 = cputime();
+    blqs::sort(data, data + SIZE);
+    printf("blqs::sort %.2fs\n", cputime() - t0);
+}
